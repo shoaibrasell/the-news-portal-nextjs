98b4774795eea2fd417c007df04aada64331d057
rasel <shoaibullahrasel@gmail.com>
2024-05-01 21:19:54 +0600

    fetch the all the

--- src/app/categories/[categoriesId]/page.js
+++ src/app/categories/[categoriesId]/page.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import { getCategoryNews } from "@/utils/getCategoryNews";
+import React from "react";
 
-const DynamicNewsPage = () => {
+const DynamicNewsPage = async ({ params, searchParams }) => {
+  // console.log(searchParams);
+  const { data } = await getCategoryNews(searchParams.category);
+  // console.log(data)
+
   return (
-    <div>
-      <h1>Dynamic News Page</h1>
+    <div className="my-5">
+      <h1>
+        Total <span className="font-bold">{searchParams.category}</span> news:{" "}
+        {data.length}{" "}
+      </h1>
     </div>
   );
 };
--- src/components/ui/CategoryList/CategoryList.js
+++ src/components/ui/CategoryList/CategoryList.js
@@ -1,5 +1,6 @@
 import { getAllCategories } from "@/utils/getAllCategories";
 import { Box, Button, Divider, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 const CategoryList = async () => {
@@ -7,20 +8,22 @@ const CategoryList = async () => {
 
   return (
     <Box className="mt-5 bg-gray-100 px-2">
-     <Typography variant="h6" textAlign={"center"}>Categories</Typography>
-     <Divider/>
-     <Stack rowGap={1} sx={{mt:2.5}}>
-      {
-        allCategories.map(category => (
-
+      <Typography variant="h6" textAlign={"center"}>
+        Categories
+      </Typography>
+      <Divider />
+      <Stack rowGap={1} sx={{ mt: 2.5 }}>
+        {allCategories.map((category) => (
           // Button input from material Ul
-
-          <Button variant="outlined" color="primary" key={category.id}>
-            {category.title}
+          <Button variant="outlined" color="success" key={category.id}>
+            <Link
+              href={`/categories/news?category=${category.title.toLowerCase()}`}
+            >
+              {category.title}
+            </Link>
           </Button>
-        ))
-      }
-     </Stack>
+        ))}
+      </Stack>
     </Box>
   );
 };
