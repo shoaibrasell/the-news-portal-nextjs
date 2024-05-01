dc50325a84f5fa4d68c9813e8ee83d8114445e0a
rasel <shoaibullahrasel@gmail.com>
2024-05-01 20:34:54 +0600

    added the category list items

--- src/components/ui/CategoryList/CategoryList.js
+++ src/components/ui/CategoryList/CategoryList.js
@@ -4,19 +4,23 @@ import React from "react";
 
 const CategoryList = async () => {
   const { data: allCategories } = await getAllCategories();
-  // console.log(allCategories);
 
   return (
-    <Box>
-      <Typography variant="h6">Categories</Typography>
-      <Divider></Divider>
-      <Stack >
-        {allCategories.map((category) => {
-          <Button variant="outlined" key={category.id}>
+    <Box className="mt-5 bg-gray-100 px-2">
+     <Typography variant="h6" textAlign={"center"}>Categories</Typography>
+     <Divider/>
+     <Stack rowGap={1} sx={{mt:2.5}}>
+      {
+        allCategories.map(category => (
+
+          // Button input from material Ul
+
+          <Button variant="outlined" color="primary" key={category.id}>
             {category.title}
-          </Button>;
-        })}
-      </Stack>
+          </Button>
+        ))
+      }
+     </Stack>
     </Box>
   );
 };
